217a90907c6dc0c1252ace9367c2f10a07be6fc5
RoberthJose <152071780+RoberthJose@users.noreply.github.com>
2025-12-15 17:15:01 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// ===== MODO OSCURO =====
+const toggleDark = document.getElementById('toggle-dark');
+
+if(localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark-mode');
+}
+
+toggleDark.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
+});
+
+// ===== MENU HAMBURGUESA =====
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+    });
+}
+
+// ===== REDIRECCIÓN FORMULARIO LOGIN =====
+const formLogin = document.querySelector('form.form-login');
+
+if(formLogin) {
+    formLogin.addEventListener('submit', (e) => {
+        e.preventDefault();
+        window.location.href = 'dashboard.html';
+    });
+}
